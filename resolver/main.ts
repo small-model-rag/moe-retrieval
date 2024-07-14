72b34d4980bb95409adafb55d48f6471d91d1c50
tobi akinyemi <tobi@akinyemi.org>
2024-07-13 19:46:58 -0700

    update

--- resolver/main.ts
+++ resolver/main.ts
@@ -6,6 +6,10 @@ import {readFile} from "fs/promises";
 const query = /*first args - string*/ process.argv[2] /*first args - string*/;
 const path = /*second arg - string*/ process.argv[3] /*second arg - string*/;
 
+if (!query || !path) {
+    throw new Error("Query and path are required - node main.js 'query' 'path'");
+}
+
 const file = await readFile(path, 'utf-8');
 const response = await select(
     query,
